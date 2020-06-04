17cb97698822d47f81c5df5571eb5ba19cd9ee7d
Jose Alves de Gouveia <gouveia32@gmail.com>
2020-06-04 20:54:31 -0300

    Cores

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
-          accentColor: Colors.green,
+          primarySwatch: Colors.purple,
+          accentColor: Colors.lightGreen,
           backgroundColor: Colors.amber,
           fontFamily: 'Lato',
           visualDensity: VisualDensity.adaptivePlatformDensity,
